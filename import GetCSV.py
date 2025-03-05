3caced7c602c3945d262d8cd85d17a21da1c4515
Yilin Wu <yilinwu588@gmail.com>
2025-03-05 16:24:14 +1100

    try different CSV files

--- import GetCSV.py
+++ import GetCSV.py
@@ -3,8 +3,8 @@ import GetCSV  # Import the module
 
 # # Define the Raw CSV file path and search terms
 # file_path = '/Users/yilinwu/Desktop/honours data/YW Trial 3 copy.csv'
-file_path = '/Users/yilinwu/Desktop/honours data/ModelOutput.csv'
-# file_path ='/Users/yilinwu/Desktop/honours data/YW-20250221/YW Trial 6.csv'
+# file_path = '/Users/yilinwu/Desktop/honours data/ModelOutput.csv'
+file_path ='/Users/yilinwu/Desktop/honours data/YW-20250221/YW Trial 6.csv'
 
 GetCSV.main(file_path)# Run the main function from GetCSV
 
@@ -12,8 +12,8 @@ import LoadExtractedData  # Import the module
 
 # # Define the extracted python file path and search terms
 # extracted_data_path = "/Users/yilinwu/Desktop/honours data/Extracted data/Yilin-YW Trial 3 copy.py"
-# extracted_data_path = "/Users/yilinwu/Desktop/honours data/Extracted data/Yilin-YW Trial 6.py"
-extracted_data_path = "/Users/yilinwu/Desktop/honours data/Extracted data/Yilin-ModelOutput.py"
+extracted_data_path = "/Users/yilinwu/Desktop/honours data/Extracted data/Yilin-YW Trial 6.py"
+# extracted_data_path = "/Users/yilinwu/Desktop/honours data/Extracted data/Yilin-ModelOutput.py"
 
 extractedData=LoadExtractedData.main(extracted_data_path)# Run the main function from LoadExtractedData
 
